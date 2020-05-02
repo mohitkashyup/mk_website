db94fbf0ede5af15baed103c4649f541591a863f
mohitk-abacus <mohitk@abacusdesk.co.in>
2020-05-02 19:27:18 +0530

    full to upload

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-aboutus',
-  templateUrl: './aboutus.component.html'
+    selector: 'app-aboutus',
+    templateUrl: './aboutus.component.html'
 })
 export class AboutusComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    
+    constructor(private titleService: Title, private metaService: Meta) { }
+    
+    ngOnInit(): void {
+        this.titleService.setTitle("Mohit Kashyup  - UI/UX and HTML Developer || Abaout Me");
+        this.metaService.addTags([
+            {name: 'keywords', content: 'mohit kashyup, mohit kashyup faridabad, html developer, ui/ux developer, cms development faridabad, mohit html developer, angular js, materializecss, angular material, bootstrap developer, 8860774311'},
+            {name: 'description', content: 'Hi, Im a UI/UX and front-end developer. I specialize in html, css, scss development. I love to build high quality responsive websites, web applications and mobile applications, 8860774311'},
+            {name: 'robots', content: 'index, follow'},
+            {property: 'og:title', content: 'Mohit Kashyup - UI/UX and HTML developer'},
+            {property: 'og:type', content: 'article'},
+            {property: 'og:url', content: 'index.html'},
+            {property: 'og:image', content: 'mohit-kashyup.jpg'},
+            {property: 'og:description', content: 'Hi, Im a UI/UX and front-end developer. I specialize in html, css, scss development. I love to build high quality responsive websites, web applications and mobile applications.,8860774311'},
+            {property: 'og:keywords', content: 'mohit kashyup, mohit kashyup faridabad, html developer, ui/ux developer, cms development faridabad, mohit html developer, angular js, materializecss, angular material, bootstrap developer,8860774311'},
+            {property: 'og:site_name', content: 'Mohit Kashyup personal website'},
+        ]);
+    }
+    
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,23 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-portfolio',
-  templateUrl: './portfolio.component.html'
+    selector: 'app-portfolio',
+    templateUrl: './portfolio.component.html'
 })
 export class PortfolioComponent implements OnInit {
-
-  constructor() {
-    this.tabActive('all');
-  }
-
-  ngOnInit(): void {
-  }
-
-  tabActiveType:any={};
-  tabActive(tab:any)
-  {
-    this.tabActiveType = {};
-    this.tabActiveType[tab] = true; 
-  }
-
+    
+    constructor(private titleService: Title, private metaService: Meta) {
+        this.tabActive('all');
+    }
+    
+    ngOnInit(): void {
+        this.titleService.setTitle("Mohit Kashyup  - UI/UX and HTML Developer || My Portfolio || My Work");
+        this.metaService.addTags([
+            {name: 'keywords', content: 'mohit kashyup, mohit kashyup faridabad, html developer, ui/ux developer, cms development faridabad, mohit html developer, angular js, materializecss, angular material, bootstrap developer, 8860774311'},
+            {name: 'description', content: 'Hi, Im a UI/UX and front-end developer. I specialize in html, css, scss development. I love to build high quality responsive websites, web applications and mobile applications, 8860774311'},
+            {name: 'robots', content: 'index, follow'},
+            {property: 'og:title', content: 'Mohit Kashyup - UI/UX and HTML developer'},
+            {property: 'og:type', content: 'article'},
+            {property: 'og:url', content: 'index.html'},
+            {property: 'og:image', content: 'mohit-kashyup.jpg'},
+            {property: 'og:description', content: 'Hi, Im a UI/UX and front-end developer. I specialize in html, css, scss development. I love to build high quality responsive websites, web applications and mobile applications.,8860774311'},
+            {property: 'og:keywords', content: 'mohit kashyup, mohit kashyup faridabad, html developer, ui/ux developer, cms development faridabad, mohit html developer, angular js, materializecss, angular material, bootstrap developer,8860774311'},
+            {property: 'og:site_name', content: 'Mohit Kashyup personal website'},
+        ]);
+    }
+    
+    tabActiveType:any={};
+    tabActive(tab:any)
+    {
+        this.tabActiveType = {};
+        this.tabActiveType[tab] = true; 
+    }
+    
 }
